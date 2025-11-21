27d97fe314743c998bebe601de64742a1ece82e7
박단현 <166380533+sodanhyun@users.noreply.github.com>
2025-11-21 16:24:01 +0900

    Initial Commit

---
+++ src/main/java/com/codehows/ksisbe/KsisBeApplication.java
@@ -0,0 +1,13 @@
+package com.codehows.ksisbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsisBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KsisBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codehows/ksisbe/KsisBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codehows.ksisbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KsisBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

489cbdb9518fefc7fc436d1fd58ea5924f3f3a4e
pky6147 <pky6147@naver.com>
2025-11-28 16:09:45 +0900

    GlobalExceptionHandler 제거
    + 관련 DTO 제거

--- src/main/java/com/codehows/ksisbe/user/controller/AuthController.java
+++ src/main/java/com/codehows/ksisbe/user/controller/AuthController.java
@@ -5,10 +5,16 @@ import com.codehows.ksisbe.user.jwt.TokenInfo;
 import com.codehows.ksisbe.user.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @Slf4j // 로깅을 위한 Lombok 어노테이션
 @RestController // RESTful 웹 서비스 컨트롤러임을 나타냅니다.
@@ -17,10 +23,15 @@ public class AuthController {
 
     private final AuthService authService;
 
-    @PostMapping("/login") // HTTP POST 요청을 "/api/auth/login" 경로에 매핑합니다.
-    public TokenInfo login(@RequestBody LoginRequestDto loginRequestDto) {
-        log.info("login request. username={}, password={}", loginRequestDto.getUsername(), loginRequestDto.getPassword());
-        TokenInfo tokenInfo = authService.login(loginRequestDto); // AuthService를 통해 로그인 처리 및 토큰 생성
-        return tokenInfo;
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequestDto loginRequestDto) {
+        try {
+            TokenInfo tokenInfo = authService.login(loginRequestDto);
+            return ResponseEntity.ok(tokenInfo);
+
+        } catch (BadCredentialsException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(Map.of("message", e.getMessage()));
+        }
     }
 }
